4deafbda451638245be451918dd23ddd8d5464ff
Thomas Bonenfant <thomasjean.bonenfant@mail.polimi.it>
2023-03-16 18:20:10 +0100

    implemented CustomEnvironmentWrapper in order to use the end effector position as action

--- air_hockey_agent/agent_builder.py
+++ air_hockey_agent/agent_builder.py
@@ -1,4 +1,6 @@
+import numpy as np
 from air_hockey_challenge.framework import AgentBase
+
 
 def build_agent(env_info, **kwargs):
     """
@@ -12,4 +14,31 @@ def build_agent(env_info, **kwargs):
          (AgentBase) An instance of the Agent
     """
 
-    raise NotImplementedError
+    return DummyAgent(env_info, **kwargs)
+
+
+class DummyAgent(AgentBase):
+    def __init__(self, env_info, **kwargs):
+        super().__init__(env_info, **kwargs)
+        self.new_start = True
+        self.hold_position = None
+
+        if kwargs['goal'] is not None:
+            self.goal = np.array(kwargs['goal'])
+        else:
+            self.goal = np.array([2,0,0])
+
+    def reset(self):
+        self.new_start = True
+        self.hold_position = None
+
+    def draw_action(self, observation):
+        if self.new_start:
+            self.new_start = False
+            self.joint_position = self.get_joint_pos(observation)
+
+        action = self.goal
+        #print(f'Joints Position: {self.hold_position}')
+        #print(f'Action: {action}')
+
+        return action
