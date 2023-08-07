c53faa3e21e2b6984a8e96ae1323caac0e5b44c3
Amirhossein zhalehmehrabi <a.h.zhalehmehraby@gmail.com>
2023-08-07 11:43:59 +0200

    submit not complete

--- air_hockey_agent/agent_builder.py
+++ air_hockey_agent/agent_builder.py
@@ -1,26 +1,17 @@
-from air_hockey_agent.agents.defend_agent import SimpleDefendingAgent
+from air_hockey_agent.agents.agents import HitAgent, DefendAgent, PrepareAgent
 from air_hockey_agent.agents.dummy_agent import DummyAgent
-
-
 def build_agent(env_info, **kwargs):
     """
     Function where an Agent that controls the environments should be returned.
     The Agent should inherit from the mushroom_rl Agent base env.
 
-    Args:
-        env_info (dict): The environment information
-        kwargs (any): Additionally setting from agent_config.yml
-    Returns:
-         (AgentBase) An instance of the Agent
+    :param env_info: The environment information
+    :return: Either Agent ot Policy
     """
-
-    agent = DummyAgent(env_info, **kwargs)
-
-    if 'agent' in kwargs:
-        if kwargs['agent'] == 'dummy-agent':
-            agent = DummyAgent(env_info, **kwargs)
-        elif kwargs['agent'] == 'defend-agent':
-            agent = SimpleDefendingAgent(env_info, **kwargs)
-
-
-    return agent
+    if "hit" in env_info["env_name"]:
+        return HitAgent(env_info, **kwargs)
+    if "defend" in env_info["env_name"]:
+        return DefendAgent(env_info, **kwargs)
+        # return DummyAgent(env_info, agent_id=1, **kwargs)
+    if "prepare" in env_info["env_name"]:
+        return PrepareAgent(env_info, **kwargs)
--- air_hockey_agent/agents/dummy_agent.py
+++ air_hockey_agent/agents/dummy_agent.py
@@ -1,36 +1,89 @@
 import numpy as np
-from air_hockey_challenge.framework.agent_base import AgentBase
+
+from envs.air_hockey_challenge.air_hockey_challenge.framework.agent_base import AgentBase
+from envs.air_hockey_challenge.air_hockey_challenge.utils import inverse_kinematics, world_to_robot, robot_to_world
+
+EE_HEIGHT = 0.0645
 
 class DummyAgent(AgentBase):
-    def __init__(self, env_info, **kwargs):
-        super().__init__(env_info, **kwargs)
-        self.new_start = True
-        self.hold_position = None
+    def __init__(self, env_info, agent_id=1, **kwargs):
 
-        self.path = kwargs['path'] if 'path' in kwargs else [[-0.8, 0, 0]]
+        super().__init__(env_info, agent_id, **kwargs)
 
-        self.steps_per_action = kwargs['steps_per_action'] if 'steps_per_action' in kwargs else 50
+        self.restart = True
+        self.last_cmd = None
+        self.joint_trajectory = None
+        self.cartesian_trajectory = None
 
-        self.step = 0
-        self.path_idx = 0
+        # Environment specs
+        self.frame = self.env_info['robot']['base_frame'][0]
+        self.table_length = self.env_info['table']['length']
+        self.table_width = self.env_info['table']['width']
+        self.goal_pos = np.array([self.table_length / 2, 0])
+        self.mallet_radius = self.env_info['mallet']['radius']
+        self.puck_radius = self.env_info['puck']['radius']
 
     def reset(self):
-        self.new_start = True
-        self.hold_position = None
+        self.restart = True
+        self.last_cmd = None
+        self.joint_trajectory = []
+        self.cartesian_trajectory = []
 
-    def draw_action(self, observation):
-        if self.new_start == True:
-            self.path_idx = 0
-            self.new_start = False
+    def draw_action(self, obs):
+        if self.restart:
+            self.restart = False
+            self.q_cmd = self.get_joint_pos(obs)
+            self.dq_cmd = self.get_joint_vel(obs)
+            self.last_cmd = np.vstack([self.q_cmd, self.dq_cmd])
+
+            self.cartesian_trajectory = self.plan_cartesian_trajectory(obs)
+
+            self.joint_trajectory = self.plan_joint_trajectory(self.cartesian_trajectory)
+
+
+        if len(self.joint_trajectory) > 0:
+            joint_pos_des = self.joint_trajectory[0]
+
+            self.joint_trajectory = self.joint_trajectory[1:]
+
+            self.last_cmd[0] = joint_pos_des
+            #self.last_cmd[1] = joint_vel_des
         else:
-            self.path_idx = (self.path_idx + 1) \
-                if self.step % self.steps_per_action == 0 and self.path_idx < len(self.path) - 1 \
-                     else self.path_idx  # updates path_idx every steps_per_action steps
-        
-        action = self.path[self.path_idx]        
+            self.last_cmd[1] = np.zeros(self.env_info['robot']['n_joints'])
 
-        #print(f'Step: {self.step} \t\t Observation: {observation}')
+        return self.last_cmd
 
-        self.step += 1
+    def plan_cartesian_trajectory(self, obs):
+        x_init, _ = robot_to_world(self.frame, [0.65, 0.0])
+        x_init = x_init[0]
+        trajectory = np.array((
+                                [x_init, 0.0, EE_HEIGHT],
+                                [x_init + 0.001, 0.0, EE_HEIGHT]
+                            ))
 
-        return np.array(action)
+        steps = 200
+        for _ in range(0, steps):
+            x = trajectory[-1][0]
+            y = trajectory[-1][1]
+            value = np.array([x+0.001, y-0.001, 0.0])
+            value[-1] = EE_HEIGHT
+
+            trajectory = np.concatenate((trajectory, [value]))
+
+        reverse_traj = trajectory[::-1]
+        trajectory = np.concatenate((trajectory, reverse_traj))
+
+        for i in range(0, len(trajectory)):
+            trajectory[i], _ = world_to_robot(self.frame, trajectory[i])
+
+        return trajectory
+
+    def plan_joint_trajectory(self, trajectory):
+        joint_pos_trajectory = np.zeros((len(trajectory), 7))
+
+        for i in range(0, len(trajectory)):
+            _, joint_pos_trajectory[i] = inverse_kinematics(self.robot_model, self.robot_data, trajectory[i])
+
+        # TODO optimize trajectory velocity
+
+        return joint_pos_trajectory
