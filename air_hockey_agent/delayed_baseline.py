7478516469a45046cdc4f99e4b6bb25224896ea7
Thomas Bonenfant <thomasjean.bonenfant@mail.polimi.it>
2023-11-23 14:23:58 +0100

    Added delayed baseline

---
+++ air_hockey_agent/delayed_baseline.py
@@ -0,0 +1,27 @@
+from air_hockey_challenge.framework import AgentBase
+from baseline.baseline_agent.baseline_agent import BaselineAgent
+import numpy as np
+
+
+class DelayedBaseline(AgentBase):
+    def __init__(self, env_info, start_time=100, **kwargs):
+        super().__init__(env_info, **kwargs)
+        self.step = 0
+        self.start_time = start_time
+        self.baseline = BaselineAgent(env_info, **kwargs)
+        self.first_position = None
+
+    def reset(self):
+        self.step = 0
+
+    def draw_action(self, observation):
+        if self.step == 0:
+            self.first_position = self.get_joint_pos(observation)
+
+        if self.step >= self.start_time:
+            action = self.baseline.draw_action(observation)
+        else:
+            action = np.vstack([self.first_position, np.zeros((7,))])
+
+        self.step += 1
+        return action
